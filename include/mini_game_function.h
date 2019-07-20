4772183c15a5dedef690a3700ce81f605b62d7ba
Nshu <d16131@ccst.numazu-ct.ac.jp>
2019-07-21 07:43:02 +0900

    fix extern miss

--- include/mini_game_function.h
+++ include/mini_game_function.h
@@ -5,7 +5,6 @@
 extern void mode_select(void);
 extern void run(int speed, int dist);
 extern void rotate(int speed, int angle);
-extern int get_number(void);
 extern int get_number(int dist);
 extern void led_on(int u_num);
 

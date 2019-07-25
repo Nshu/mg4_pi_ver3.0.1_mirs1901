33e4a0ce8db59a0de58cbebe942486d205e975d2
Nshu <35136066+Nshu@users.noreply.github.com>
2019-07-25 16:51:55 +0900

    Update mini_game_function.h
    prototype definition of led_off()

--- include/mini_game_function.h
+++ include/mini_game_function.h
@@ -7,5 +7,6 @@ extern void run(int speed, int dist);
 extern void rotate(int speed, int angle);
 extern int get_number(int dist);
 extern void led_on(int u_num);
+extern void led_off(void);
 
 #endif //MG4_PI_VER3_0_1_MINI_GAME_FUNCTION_H
